f1516b4d28ee1d7dc33da40e963286b73257a29d
CkhGueye <alabaygueye@gmail.com>
2023-04-09 14:32:22 +0000

    Progressive Web App config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,37 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint()],
+  plugins: [
+    react(),
+    eslint(),
+    VitePWA({
+      manifest: {
+        name: "My Timer App",
+        short_name: "Timer App",
+        start_url: "/ReactTimerApp/index.html",
+        scope: "https://ckhgueye.github.io/ReactTimerApp/",
+        display: "standalone",
+        background_color: "#e3edf7",
+        theme_color: "#555",
+        icons: [
+          {
+            src: "icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
+  base: "https://ckhgueye.github.io/ReactTimerApp/",
 });
